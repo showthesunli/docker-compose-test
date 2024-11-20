92fa2767e7f3e4eb8a4d774835b113f3e9136a0c
showthesunli <showthesunli@gmail.com>
2024-11-21 01:33:45 +0800

    test env

--- next-app/src/app/envtest/page.tsx
+++ next-app/src/app/envtest/page.tsx
@@ -1,4 +1,10 @@
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  console.log("searchParams.a :>> ", searchParams.a);
+  console.log("searchParams.b :>> ", searchParams.b);
   console.log("process.env.LEEORZ in app dir :>> ", process.env.LEEORZ);
   return <div>page</div>;
 }
