9e2915f4e481f61bb19460ac1a2503a215d1babe
showthesunli <showthesunli@gmail.com>
2024-11-28 03:10:02 +0800

    test pro

--- next-app/src/app/envtest/page.tsx
+++ next-app/src/app/envtest/page.tsx
@@ -6,5 +6,9 @@ export default function Page({
   console.log("searchParams.a :>> ", searchParams.a);
   console.log("searchParams.b :>> ", searchParams.b);
   console.log("process.env.LEEORZ in app dir :>> ", process.env.LEEORZ);
+  console.log(
+    "process.env.NEXT_PUBLIC_ENV_VARIABLE :>> ",
+    process.env.NEXT_PUBLIC_ENV_VARIABLE
+  );
   return <div>page</div>;
 }
